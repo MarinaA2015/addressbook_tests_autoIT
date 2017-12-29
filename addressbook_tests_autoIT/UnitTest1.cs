d180ccd3474d860b9b0a3ec3ce08934186220ea8
Marina <m.abrashkina2015@gmail.com>
2017-12-29 16:20:00 +0200

    Add project files.

---
+++ addressbook_tests_autoIT/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace addressbook_tests_autoIT
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
